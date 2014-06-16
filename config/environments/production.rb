1d155a5458b6199265f48d68be506ac86119812d
Bharadwaj Krishnan <bharadwaj.krishnan@gmail.com>
2014-06-16 18:38:39 +0530

    image load error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Noteshare::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -77,4 +77,6 @@ Noteshare::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 end
